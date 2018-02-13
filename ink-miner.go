0c75a448438eae888b186f0df9b5565e77d2b931
Ji Yoon Ok <jiyoonok@Jis-MacBook-Pro.local>
2018-02-13 11:28:42 -0800

    added selectRandomBranch, getlongestblockchain, checkintersection methods, added point struct

--- ink-miner.go
+++ ink-miner.go
@@ -16,13 +16,13 @@ import (
 	"crypto/ecdsa"
 	"crypto/elliptic"
 	"crypto/md5"
-	"crypto/rand"
 	"crypto/x509"
 	"encoding/gob"
 	"encoding/hex"
 	"encoding/json"
 	"errors"
 	"fmt"
+	"math"
 	"math/big"
 	mrand "math/rand"
 	"net"
@@ -87,6 +87,11 @@ type Operation struct {
 
 type LongestBlockChain struct {
 	BlockChain []Block
+}
+
+type Point struct {
+	x float64
+	y float64
 }
 
 // Keeps track of all the keys & Miner Address so miner can send it to other miners.
@@ -235,12 +240,6 @@ func (artkey *ArtKey) GetInk(_ *struct{}, inkAmount *uint32) error {
 	return nil
 }
 
-// TODO
-func validatedWithNetwork(block Block) bool {
-	// TODO
-	return true
-}
-
 func GetInkAmount(prevBlock *Block) uint32 {
 	temp := *prevBlock
 	for i := prevBlock.PathLength; i > 1; i-- {
@@ -279,13 +278,13 @@ func GenerateBlock(settings blockartlib.MinerNetSettings) {
 		endBlocks := FindLastBlockOfLongestChain()
 
 		if len(endBlocks) > 1 {
-			if !isNoop {
+			if !isNoOp {
 				prevBlock = SelectValidBranch(endBlocks, newBlock)
 			} else {
 				prevBlock = SelectRandomBranch(endBlocks)
 			}
 		} else {
-			prevBlock = *endBlocks[0]
+			prevBlock = endBlocks[0]
 		}
 
 		prevBlockHash := ComputeBlockHash(*prevBlock)
@@ -308,6 +307,138 @@ func GenerateBlock(settings blockartlib.MinerNetSettings) {
 			newBlock.Nonce = newBlock.Nonce + 1
 		}
 	}
+}
+
+// Select valid branch among > 1 longest nodes
+// If all branches are valid, then choose one at random
+func SelectValidBranch(endBlocks []*Block, newBlock Block) *Block {
+	var validEndBlocks []*Block
+	for _, block := range endBlocks {
+		currPath := FindBlockChainPath(block)
+		isValid := ValidateOperationAgainstPath(newBlock.SetOPs, currPath)
+		if isValid {
+			validEndBlocks = append(validEndBlocks, block)
+		}
+	}
+
+	if len(validEndBlocks) > 1 {
+		return SelectRandomBranch(validEndBlocks)
+	} else if len(validEndBlocks) == 1 {
+		return validEndBlocks[0]
+	}
+	return nil
+}
+
+// Checks if given branch causes any validation error for given set of operations
+func ValidateOperationAgainstPath(ops []Operation, block []Block) bool {
+	// TODO
+	return true
+}
+
+// Selects random node in the given array
+func SelectRandomBranch(endBlocks []*Block) *Block {
+	length := len(endBlocks)
+	mrand.Seed(time.Now().Unix())
+	randIndex := mrand.Intn(length - 1)
+
+	return endBlocks[randIndex]
+}
+
+// Checks for any possible intersection between set of operations and operations in
+// the longest block chain
+func CheckIntersection(operation Operation) bool {
+	deletes := []Operation{}
+	blockChain := FindLongestBlockChain()
+	point1 := Point{x: operation.xStart, y: operation.yStart}
+	point2 := Point{x: operation.xEnd, y: operation.yEnd}
+	for k := len(blockChain); k > 0; k-- {
+		for _, op := range blockChain[k].SetOPs {
+			point3 := Point{x: op.xStart, y: op.yStart}
+			point4 := Point{x: op.xEnd, y: op.yEnd}
+			if CheckIntersectionLines(point1, point2, point3, point4) {
+				if op.OpType == "Add" {
+					exists := false
+					for i, delOp := range deletes {
+						if delOp.xStart == op.xStart && delOp.xEnd == op.xEnd &&
+							delOp.xEnd == op.xEnd && delOp.yEnd == op.yEnd {
+							deletes = append(deletes[:i], deletes[i+1:]...)
+							exists = true
+							break
+						}
+					}
+					if !exists {
+						return false
+					}
+				} else {
+					deletes = append(deletes, op)
+				}
+			}
+		}
+	}
+	return false
+}
+
+// Checks if given set of points intersect
+// Logic sourced from www.geeksforgeeks.org/check-if-two-given-line-segments-intersect/
+func CheckIntersectionLines(p1 Point, p2 Point, p3 Point, p4 Point) bool {
+	o1 := Orientation(p1, p2, p3)
+	o2 := Orientation(p1, p2, p4)
+	o3 := Orientation(p3, p4, p1)
+	o4 := Orientation(p3, p4, p2)
+
+	if o1 != o2 && o3 != o4 {
+		return true
+	}
+
+	if o1 == 0 && OnSegment(p1, p3, p2) {
+		return true
+	}
+	if o2 == 0 && OnSegment(p1, p4, p2) {
+		return true
+	}
+	if o3 == 0 && OnSegment(p3, p1, p4) {
+		return true
+	}
+	if o4 == 0 && OnSegment(p3, p2, p4) {
+		return true
+	}
+	return false
+}
+
+// Find orientation of the triplet points
+// 1 -> c1, c2, c3 are colinear
+// 2 -> Clockwise
+// 3 -> Counterclockwise
+func Orientation(c1 Point, c2 Point, c3 Point) int {
+	val := (c2.y-c1.y)*(c3.x-c2.x) - (c2.x-c1.x)*(c3.y-c2.y)
+
+	if val == 0 {
+		return 0
+	} else if val > 0 {
+		return 1
+	} else {
+		return 2
+	}
+}
+
+// Checks if point c2 lies on the line segment between c1 and c3
+func OnSegment(c1 Point, c2 Point, c3 Point) bool {
+	if c2.x <= math.Max(c1.x, c3.x) && c2.x >= math.Min(c1.x, c3.x) &&
+		c2.y <= math.Max(c1.y, c3.y) && c2.y >= math.Min(c1.y, c3.y) {
+		return true
+	}
+	return false
+}
+
+func FindLongestBlockChain() []Block {
+	endNodes := FindLastBlockOfLongestChain()
+	if len(endNodes) > 1 {
+		// TODO : for edge case when multiple longest branches are present
+		// we will select at random for now
+		randNode := SelectRandomBranch(endNodes)
+		return FindBlockChainPath(randNode)
+	}
+	return FindBlockChainPath(endNodes[0])
 }
 
 func FindLastBlockOfLongestChain() []*Block {
@@ -399,7 +530,7 @@ func (minerKey *MinerKey) ReceiveBlock(block *Block, reply *string) error {
 		}
 	case 2:
 		if ComputeTrailingZeroes(hash, settings.PoWDifficultyOpBlock) {
-			for _,operation := range receivedBlock.SetOPs{
+			for _, operation := range receivedBlock.SetOPs {
 				ValidateOperation(operation)
 			}
 		} else {
@@ -422,7 +553,7 @@ func (minerKey *MinerKey) ReceiveBlock(block *Block, reply *string) error {
 
 	blockList = append(blockList, receivedBlock)
 	err := SendBlockInfo(receivedBlock)
-	return er
+	return nil
 }
 
 // Floods the network of miners with Operations
@@ -479,7 +610,6 @@ func CheckPreviousBlock(hash string) (*Block, bool) {
 	return nil, false
 }
 
-
 // call this for op-blocks to validate the op-block
 func ValidateOperation(operation Operation) error {
 
@@ -488,34 +618,33 @@ func ValidateOperation(operation Operation) error {
 	// Check that each operation in the block has a valid signature
 
 	if ecdsa.Verify(&operation.ArtNodePubKey, []byte(GlobalHash), operation.OPSigR, operation.OPSigS) {
-			fmt.Println("op-sig is valid .... continuing validation")
-		} else {
-			return errors.New("failed to validate operation signature")
-		}
+		fmt.Println("op-sig is valid .... continuing validation")
+	} else {
+		return errors.New("failed to validate operation signature")
+	}
 
-	if operation.OpType == "Delete"{
-			for _, doneOp := range operationsHistory {
-				if operation.UniqueID == doneOp {
-					fmt.Println("Delete operation validation sucess, shape to be deleted exists")
-				} else {
-					return errors.New("Delete operation could not find a shape previously added")
-				}
+	if operation.OpType == "Delete" {
+		for _, doneOp := range operationsHistory {
+			if operation.UniqueID == doneOp {
+				fmt.Println("Delete operation validation sucess, shape to be deleted exists")
+			} else {
+				return errors.New("Delete operation could not find a shape previously added")
 			}
 		}
+	}
 
-	if operation.OpType == "Add"{
-			for _, doneOp := range operationsHistory {
-				if operation.UniqueID == doneOp {
-					return errors.New("Duplicate add operation of same shape")
-				} else {
-					fmt.Println("identical signature could not be found, add shape validation sucess")
-				}
+	if operation.OpType == "Add" {
+		for _, doneOp := range operationsHistory {
+			if operation.UniqueID == doneOp {
+				return errors.New("Duplicate add operation of same shape")
+			} else {
+				fmt.Println("identical signature could not be found, add shape validation sucess")
 			}
 		}
-
+	}
 
 	// checking ink amount
-	for _,block := range blockList{
+	for _, block := range blockList {
 		if block.MinerPubKey == operation.ArtNodePubKey {
 			minerCurrentInk := GetInkAmount(&block)
 			minerCurrentInk = minerCurrentInk - operation.OpInkCost
@@ -528,7 +657,7 @@ func ValidateOperation(operation Operation) error {
 			return errors.New("ArtNodePubKey's associated MinerPubKey could not be found")
 		}
 	}
-	return errors.New("failed to validate operation")
+	return nil
 }
 
 // HELPER FUNCTIONS
